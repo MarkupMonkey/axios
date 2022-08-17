03babd9004588a58060892332fc6e4e9b7e5184a
Marco Moncada <99331256+MarkupMonkey@users.noreply.github.com>
2022-08-18 00:03:03 +0200

    Interceptors

---
+++ src/axios/interceptors.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const authFetch = axios.create({
+    baseURL: 'https://course-api.com',
+
+});
+
+authFetch.interceptors.request.use(
+    (request) => {
+        request.headers.common['Accept'] = 'application/json'
+        console.log('request sent')
+        return request;
+    },
+    (error) => {
+        return Promise.reject(error)
+    })
+
+authFetch.interceptors.request.use(() => { }, () => {
+    console.log('got response');
+    return Response;
+},
+    (error) => {
+        console.log(error.response)
+        if (error.response.this.status === 404) {
+            // do something
+            console.log('NOT FOUND');
+        }
+        return Promise.reject(error)
+    })
+
+export default authFetch;
